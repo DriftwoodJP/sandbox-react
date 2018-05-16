37459eb7bad4bf76b7e74432ff4921c7cbb88d82
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2018-05-16 13:59:44 +0900

    Chapter 5

--- dest/app.js
+++ dest/app.js
@@ -1,7 +1,27 @@
-'use strict';
+"use strict";
 
-ReactDOM.render(React.createElement(
-    'h1',
+//コンポーネントの作成
+function MyH1() {
+  return React.createElement(
+    "h1",
     null,
-    'Hello, world!'
-), document.getElementById('app'));
+    "Hello, world!"
+  );
+}
+function MyH2() {
+  return React.createElement(
+    "h2",
+    null,
+    "texttext"
+  );
+}
+function MyComponent() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(MyH1, null),
+    React.createElement(MyH2, null)
+  );
+}
+//コンポーネントの描画
+ReactDOM.render(React.createElement(MyComponent, null), document.getElementById("app"));
--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,17 @@
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('app')
-);
+//コンポーネントの作成
+function MyH1() {
+  return <h1>Hello, world!</h1>;
+}
+function MyH2() {
+  return <h2>texttext</h2>;
+}
+function MyComponent() {
+  return (
+    <div>
+      <MyH1 />
+      <MyH2 />
+    </div>
+  );
+}
+//コンポーネントの描画
+ReactDOM.render(<MyComponent />, document.getElementById("app"));
