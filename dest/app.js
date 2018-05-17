608df03868b43743d410adaf401a307bb06f295e
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2018-05-17 11:46:36 +0900

    Chapter 9

--- dest/app.js
+++ dest/app.js
@@ -9,30 +9,65 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 //コンポーネントの作成
-var MyComponent = function (_React$Component) {
-  _inherits(MyComponent, _React$Component);
+var Number = function (_React$Component) {
+  _inherits(Number, _React$Component);
 
-  function MyComponent(props) {
-    _classCallCheck(this, MyComponent);
+  function Number(props) {
+    _classCallCheck(this, Number);
 
-    var _this = _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Number.__proto__ || Object.getPrototypeOf(Number)).call(this, props));
 
     _this.state = {
-      myText: null
+      stateVal: 0
     };
-    _this.setMyText = _this.setMyText.bind(_this);
+    _this.incrementState = _this.incrementState.bind(_this);
     return _this;
   }
-  //テキストの設定
+  //Stateの加算
 
 
-  _createClass(MyComponent, [{
-    key: "setMyText",
-    value: function setMyText() {
+  _createClass(Number, [{
+    key: "incrementState",
+    value: function incrementState() {
       this.setState({
-        myText: this.refs.myText.value
+        stateVal: this.state.stateVal + 1
       });
-      this.refs.myText.value = "";
+    }
+  }, {
+    key: "componentWillMount",
+    value: function componentWillMount() {
+      console.log("componentWillMount(): コンポーネントのマウント前");
+    }
+  }, {
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      console.log("componentDidMount(): コンポーネントのマウント後");
+    }
+  }, {
+    key: "componentWillReceiveProps",
+    value: function componentWillReceiveProps() {
+      console.log("componentWillReceiveProps(): コンポーネントが受け取るpropsが変化");
+    }
+  }, {
+    key: "shouldComponentUpdate",
+    value: function shouldComponentUpdate() {
+      console.log("shouldComponentUpdate(): コンポーネントがアップデートされる前1");
+      return true;
+    }
+  }, {
+    key: "componentWillUpdate",
+    value: function componentWillUpdate() {
+      console.log("componentWillUpdate(): コンポーネントがアップデートされる前2");
+    }
+  }, {
+    key: "componentDidUpdate",
+    value: function componentDidUpdate() {
+      console.log("componentDidUpdate(): コンポーネントがアップデートされた後");
+    }
+  }, {
+    key: "componentWillUnmount",
+    value: function componentWillUnmount() {
+      console.log("componentWillUnmount(): コンポーネントがアンマウントされた後");
     }
   }, {
     key: "render",
@@ -43,11 +78,79 @@ var MyComponent = function (_React$Component) {
         React.createElement(
           "p",
           null,
-          "\u5165\u529B\u5024:",
-          this.state.myText
+          "props:",
+          this.props.propVal
         ),
-        React.createElement("input", { type: "text", ref: "myText" }),
-        React.createElement("input", { type: "button", value: "\u5165\u529B", onClick: this.setMyText })
+        React.createElement(
+          "p",
+          null,
+          "state:",
+          this.state.stateVal
+        ),
+        React.createElement("input", { type: "button", value: "state\u306E\u52A0\u7B97", onClick: this.incrementState })
+      );
+    }
+  }]);
+
+  return Number;
+}(React.Component);
+
+var MyComponent = function (_React$Component2) {
+  _inherits(MyComponent, _React$Component2);
+
+  function MyComponent(props) {
+    _classCallCheck(this, MyComponent);
+
+    var _this2 = _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this, props));
+
+    _this2.state = {
+      propVal: 0,
+      isComponent: false
+    };
+    _this2.incrementProps = _this2.incrementProps.bind(_this2);
+    _this2.ｍountComponent = _this2.ｍountComponent.bind(_this2);
+    return _this2;
+  }
+  //propsの加算
+
+
+  _createClass(MyComponent, [{
+    key: "incrementProps",
+    value: function incrementProps() {
+      this.setState({
+        propVal: this.state.propVal + 1
+      });
+    }
+  }, {
+    key: "\uFF4DountComponent",
+    value: function ountComponent() {
+      this.setState({
+        isComponent: !this.state.isComponent
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this3 = this;
+
+      return React.createElement(
+        "div",
+        null,
+        React.createElement("input", {
+          type: "button",
+          value: "Number\u30B3\u30F3\u30DD\u30FC\u30CD\u30F3\u30C8\u306E\u30DE\u30A6\u30F3\u30C8\u5207\u308A\u66FF\u3048",
+          onClick: this.ｍountComponent
+        }),
+        React.createElement("br", null),
+        function () {
+          if (_this3.state.isComponent) {
+            return React.createElement(Number, { propVal: _this3.state.propVal });
+          } else {
+            return null;
+          }
+        }(),
+        React.createElement("input", { type: "button", value: "props\u306E\u52A0\u7B97", onClick: this.incrementProps }),
+        React.createElement("br", null)
       );
     }
   }]);
--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,91 @@
 //コンポーネントの作成
-class MyComponent extends React.Component {
+class Number extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      myText: null
+      stateVal: 0
     };
-    this.setMyText = this.setMyText.bind(this);
+    this.incrementState = this.incrementState.bind(this);
   }
-  //テキストの設定
-  setMyText() {
+  //Stateの加算
+  incrementState() {
     this.setState({
-      myText: this.refs.myText.value
+      stateVal: this.state.stateVal + 1
     });
-    this.refs.myText.value = "";
+  }
+  componentWillMount() {
+    console.log("componentWillMount(): コンポーネントのマウント前");
+  }
+  componentDidMount() {
+    console.log("componentDidMount(): コンポーネントのマウント後");
+  }
+  componentWillReceiveProps() {
+    console.log("componentWillReceiveProps(): コンポーネントが受け取るpropsが変化");
+  }
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate(): コンポーネントがアップデートされる前1");
+    return true;
+  }
+  componentWillUpdate() {
+    console.log("componentWillUpdate(): コンポーネントがアップデートされる前2");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate(): コンポーネントがアップデートされた後");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount(): コンポーネントがアンマウントされた後");
   }
   render() {
     return (
       <div>
-        <p>入力値:{this.state.myText}</p>
-        <input type="text" ref="myText" />
-        <input type="button" value="入力" onClick={this.setMyText} />
+        <p>props:{this.props.propVal}</p>
+        <p>state:{this.state.stateVal}</p>
+        <input type="button" value="stateの加算" onClick={this.incrementState} />
+      </div>
+    );
+  }
+}
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      propVal: 0,
+      isComponent: false
+    };
+    this.incrementProps = this.incrementProps.bind(this);
+    this.ｍountComponent = this.ｍountComponent.bind(this);
+  }
+  //propsの加算
+  incrementProps() {
+    this.setState({
+      propVal: this.state.propVal + 1
+    });
+  }
+
+  ｍountComponent() {
+    this.setState({
+      isComponent: !this.state.isComponent
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="button"
+          value="Numberコンポーネントのマウント切り替え"
+          onClick={this.ｍountComponent}
+        />
+        <br />
+        {(() => {
+          if (this.state.isComponent) {
+            return <Number propVal={this.state.propVal} />;
+          } else {
+            return null;
+          }
+        })()}
+        <input type="button" value="propsの加算" onClick={this.incrementProps} />
+        <br />
       </div>
     );
   }
