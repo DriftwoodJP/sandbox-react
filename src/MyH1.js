9749dcaebc01618fa218b554fbf893421f15bd31
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2018-05-18 17:34:34 +0900

    Chapter 11

---
+++ src/MyH1.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function MyH1() {
+  return <h1>Hello, world!</h1>;
+}
---
+++ src/MyH2.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function MyH2() {
+  return <h2>texttext</h2>;
+}
